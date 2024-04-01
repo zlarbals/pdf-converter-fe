ea9822884706380119fce8156fce2a50db567616
zlarbals <zlarbals@gmail.com>
2024-04-02 00:58:00 +0900

    feat: 파일 드래그&드랍 기능 구현

--- src/components/FileConvert.jsx
+++ src/components/FileConvert.jsx
@@ -1,54 +1,55 @@
-import { useState } from "react"
+import {useState} from "react"
+import UploadBox from "./UploadBox";
 
 function FileConvert() {
-  const [inputFile, setInputFile] = useState(null);
+    const [inputFile, setInputFile] = useState(null);
 
-  const handleFileChange = (e) => {
-    setInputFile(e.target.files[0]);
-  };
+    const handleFileChange = (file) => {
+        setInputFile(file);
+    };
 
-  const handleUpload = async () => {
-    if(!inputFile){
-      console.log('파일을 선택하세요.');
-      return;
-    }
+    const handleUpload = async () => {
+        if (!inputFile) {
+            console.log('파일을 선택하세요.');
+            return;
+        }
 
-    const formData = new FormData();
-    formData.append('file', inputFile);
+        const formData = new FormData();
+        formData.append('file', inputFile);
 
-    try {
-      const response = await fetch('http://localhost:8080/convert/convertToPdf',{
-        method: 'POST',
-        body: formData
-      });
+        try {
+            const response = await fetch('http://localhost:8080/convert/convertToPdf', {
+                method: 'POST',
+                body: formData
+            });
 
-      console.log(response);
-
-      if(response.ok){
-        const byteArray = await response.arrayBuffer();
-        const blob = new Blob([byteArray]);
-        const url = URL.createObjectURL(blob);
-        const downloadLink = document.createElement('a');
-        downloadLink.href = url;
-        downloadLink.setAttribute('download','ConvertFile.pdf');
-        document.body.appendChild(downloadLink);
-        downloadLink.click();
-        document.body.removeCheld(downloadLink);
-      }else{
-        console.error('파일 변환에 실패했습니다.');
-      }
-    }catch(error){
-      console.error('네트워크 오류', error);
-    }
-  };
+            if (response.ok) {
+                const byteArray = await response.arrayBuffer();
+                const blob = new Blob([byteArray]);
+                const url = URL.createObjectURL(blob);
+                const downloadLink = document.createElement('a');
+                downloadLink.href = url;
+                downloadLink.setAttribute('download', 'ConvertFile.pdf');
+                document.body.appendChild(downloadLink);
+                downloadLink.click();
+                document.body.removeCheld(downloadLink);
+            } else {
+                console.error('파일 변환에 실패했습니다.');
+            }
+        } catch (error) {
+            console.error('네트워크 오류', error);
+        }
+    };
 
 
-  return (
-    <div>
-      <input type="file" onChange={handleFileChange} />
-      <button onClick={handleUpload}>pdf 변환</button>
-    </div>
-  )
+    return (
+        <div>
+            <UploadBox onFileChange={handleFileChange}/>
+            <div>
+                <button onClick={handleUpload}>pdf 변환</button>
+            </div>
+        </div>
+    )
 }
 
 export default FileConvert
---
+++ src/components/UploadBox.jsx
@@ -0,0 +1,55 @@
+import {useState} from "react"
+import './styles.css'
+import FileInfo from "./FileInfo.jsx";
+
+function UploadBox({onFileChange}) {
+    const [isActive, setActive] = useState(false);
+    const [uploadFileInfo, setUploadFileInfo] = useState(null);
+    const handleDragStart = () => {
+        setActive(true);
+    }
+
+    const handleDragEnd = () => {
+        setActive(false);
+    }
+
+    const handleDragOver = (e) => {
+        e.preventDefault();
+    }
+
+    const handleDrop = (e) => {
+        e.preventDefault();
+
+        const file = e.dataTransfer.files[0];
+        console.log(file);
+        onFileChange(file);
+        setActive(false);
+        setFileInfo(file);
+    }
+
+    const setFileInfo = (file) => {
+        const {name, size: byteSize, type} = file;
+        const size = (byteSize / (1024 * 1024)).toFixed(2) + 'mb';
+        setUploadFileInfo({name, size, type});
+    }
+
+
+    return (
+        <label className={`uploadBox${isActive ? ' active' : ''}`}
+               onDragEnter={handleDragStart}
+               onDragOver={handleDragOver}
+               onDragLeave={handleDragEnd}
+               onDrop={handleDrop}>
+
+            <input type="file" className="uploadBox_file"/>
+            {uploadFileInfo && <FileInfo uploadFileInfo={uploadFileInfo}/>}
+
+            {!uploadFileInfo && (
+                <p>클릭 혹은 파일을 이곳에 드랍하세요.</p>
+            )}
+
+        </label>
+    )
+}
+
+export default UploadBox
